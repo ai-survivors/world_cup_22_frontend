e6b770724cddda405411853a47454ecc381e7aac
mhmad wrekat <mhmmadwreekkat@gmail.com>
2022-01-01 13:58:06 +0200

    add

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <section>
         <Head>
             <title>World Cup 2022</title>
-            <link rel="icon" href="/trophy.png" />
+            <link rel="icon" href="./trophy.png" />
         </Head>
 <div className="leading-normal tracking-normal text-white gradient">
 <nav id="header" className="fixed top-0 z-30 w-full text-gray-300 bg-pink-800">
@@ -17,6 +17,7 @@ const Header = () => {
 <svg className="inline h-9" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 <rect fill="#2a2a31" x="16.539" y="425.626" width="479.767" height="50.502" transform="matrix(1,0,0,1,0,0)" />
 <path className="plane-take-off" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+<title>Main Page</title>
 </svg>
 {' '}World Cup
 </a>
