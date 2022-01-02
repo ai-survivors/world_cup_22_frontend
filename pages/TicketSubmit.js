fc46614064234dd9fbe2689e5e38d15b65a4320f
tradalhariri <trad.alhariri@gmail.com>
2022-01-02 19:45:43 +0200

    fix buy ticket

--- pages/TicketSubmit.js
+++ pages/TicketSubmit.js
@@ -3,18 +3,31 @@ import Footer from "../components/Footer";
 import { useAuth } from '../contexts/auth'
 import useTickets from "../hooks/useTickets";
 import QRCode from "qrcode.react";
+import { useEffect,useState } from "react";
 
 
 
 const TicketSubmit = () => {
-const {user , login , logout} = useAuth()
+const {user , login , logout,tokens} = useAuth()
 const {ticketResources,  ticketLoading, createTicket } = useTickets();
+console.log("tickets ",ticketResources);
+console.log("tokens ",tokens);
+
+const [tickets,setTickets ] = useState([])
+
+useEffect(()=>{
+   if (ticketResources != undefined){
+     setTickets(ticketResources)
+   }
+},[ticketResources,tickets])
+
+console.log("tickets ",tickets);
 
   return (
     <>
       <Header />
       
-      { !ticketLoading &&  ticketResources.map((data) => {
+      { tickets.map((data) => {
         
             if (data.owner==user.id){
               return (
@@ -23,16 +36,16 @@ const {ticketResources,  ticketLoading, createTicket } = useTickets();
                 
                 <div className="bg-[url('https://images.assetsdelivery.com/compings_v2/gavrby/gavrby2104/gavrby210400021.jpg')]  mt-10 w-2/3 mr-50 h-80  rounded-3xl bg-cover " >
                   <div>
-                    <div className=" pt-5 pb-5 pl-20 pr-20 rounded-3xl">
+                    <div className="pt-5 pb-5 pl-20 pr-20 rounded-3xl">
                     <img className='hover:scale-110 cursor-grab' src='https://github.com/ai-survivors/world_cup_22_frontend/raw/main/assest/logo.png' width='150'/> 
-                      <h2 className=" font-mono text-3xl font-extrabold tracking-tight text-white sm:text-4xl mt-50">
+                      <h2 className="font-mono text-3xl font-extrabold tracking-tight text-white sm:text-4xl mt-50">
                         World Cup Ticket{" "}
                       </h2>
                     </div>
                     <div className="float-right pr-6">
                       <QRCode id="abc" value="rr" />
                     </div>
-                    <div className=" font-mono  ml-20 flex flex-row  text-white w-auto">
+                    <div className="flex flex-row w-auto ml-20 font-mono text-white ">
                    
                      <div className="basis-1/2"> Match between: {data.match}</div>
                      <div className="basis-1/2"> Price:{data.price} </div>
@@ -42,7 +55,7 @@ const {ticketResources,  ticketLoading, createTicket } = useTickets();
                    
                     </div>
                     
-                    <div className=" font-mono  ml-20 mt-2 flex flex-row  text-white w-auto">
+                    <div className="flex flex-row w-auto mt-2 ml-20 font-mono text-white ">
                     <div className="basis-1/2" >Date:{data.created_date}</div>
                      <div className="basis-1/2">Seat:19-Zone: A</div>
                   
